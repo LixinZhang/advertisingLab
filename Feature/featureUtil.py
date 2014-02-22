0a3a1efa16ef04bd57a3341a51d6e5a499a1a3d5
Zhang Lixin <zhanglixinseu@gmail.com>
2014-02-22 22:36:57 +0800

    fixbug

--- Feature/featureUtil.py
+++ Feature/featureUtil.py
@@ -80,7 +80,7 @@ def joinResult4SVMRanking(fn_trainFeature=TMP_DATA_DIR_PATH+'LDA_corpus.svmlight
         for userid in userids :
             if userid not in userFeature or (adid, userid) not in statusMap:
                 continue
-        output.write(format % (statusMap[(adid, userid)], adid2Idx[adid], userFeature[userid]))
+            output.write(format % (statusMap[(adid, userid)], adid2Idx[adid], userFeature[userid]))
 
     output.close()
 
